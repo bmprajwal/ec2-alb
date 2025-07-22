bef3ce3e17db47e74f5f039ab6f71d12196a8031
BM Prajwal <prajwalbm2003@gmail.com>
2025-07-22 15:09:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ variable "subnet_ids" {
   description = "List of subnet IDs for ALB and instances"
   type        = list(string)
 }
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
