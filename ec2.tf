107264f3ba3211a9c0c3bba2a0879d32e90c58b7
BM Prajwal <prajwalbm2003@gmail.com>
2025-07-22 15:05:35 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx_sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "HTTP from ALB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = [aws_lb.main.security_group_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  count                       = 2
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = element(var.subnet_ids, count.index)
+  vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install nginx1 -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "nginx-instance-${count.index + 1}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nginx_tg_attach" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.nginx_tg.arn
+  target_id        = aws_instance.nginx[count.index].id
+  port             = 80
+}
